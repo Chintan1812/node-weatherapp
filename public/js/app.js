e6a35b4786f82c0e45889d493b3c23b8d9b6b1f1
Chintan Bhayani <bhayanichintan14@gmail.com>
2020-05-04 18:14:31 +0530

    Deploying commit

--- public/js/app.js
+++ public/js/app.js
@@ -9,14 +9,12 @@ weatherForm.addEventListener('submit',(e)=>{
     const div = document.getElementById('forecast');
     div.innerHTML = "";
     const location = address.value;
-    fetch(`http://localhost:3000/weather?address=${location}`).then((res)=> {
+    fetch(`/weather?address=${location}`).then((res)=> {
     res.json().then((data)=> {
         const errorHTML = `<br><p>Error: <span id="locationRes"></span></p>`
         if(data.error){
-            loading.innerHTML = " ";
             document.getElementById("forecast").insertAdjacentHTML('afterbegin', errorHTML);
         }else {
-            loading.innerHTML = " ";
             const dataHTML = `<br><p><strong>Forecast: </strong><span>${data.forecast}</span></p>`
             document.getElementById("forecast").insertAdjacentHTML('afterbegin', dataHTML);
         }
